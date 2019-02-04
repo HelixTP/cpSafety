ca05c08f87456a651200286c6cddb34e7f480db3
Helix <dlallemand@gmail.com>
2019-02-04 21:04:05 +0100

    use ncurses lib

--- src/main.c
+++ src/main.c
@@ -3,25 +3,33 @@
 //
 
 #include <stdio.h>
+#include <string.h>
 #include <dirent.h>
+
+#include <ncurses.h>
 
 void ls_dir(char*nameDir);
 
 int main (int argc, char *argv[])
 {
+    initscr();
+
     if(argc > 1){
-        printf("nombre d'argument : %d\n",argc);
+        printw("nombre d'argument : %d\n",argc);
         for (int i = 1;i < argc; ++i) {
-            printf("%s\t",argv[i]);
+            printw("%s\t",argv[i]);
         }
     } else {
-        printf("Pas d'argument \n");
+        printw("Pas d'argument \n");
     }
-    printf("\n");
-    printf("--- Begin copy ---\n");
+    printw("\n");
+    printw("--- Begin copy ---\n");
     ls_dir(argv[1]);
-    printf("--- End copy ---\n");
-
+    printw("--- End copy ---\n");
+    
+    refresh(); 
+    getch();
+    endwin();
     return 0;
 }
 
@@ -33,17 +41,10 @@ void ls_dir(char*nameDir){
     if(dir){
         while( (d=readdir(dir)) ){
             if (!((!(strcmp(d->d_name,"."))) || (!(strcmp(d->d_name,"..")))) ) {
-                /* code */
-                if (d->d_name[0]=='.') {
-                printf("Hidden file - ");
+                if (!(d->d_name[0]=='.')) {
+                    printw("%s and dir type %d\n ",d->d_name, d->d_type);
                 }              
-                printf("%s and dir type %d\n ",d->d_name, d->d_type);
-
-            }else
-            {
-               printf("rejetc  . and .. - ");
-               printf("%s\n ",d->d_name);
-            }
+            } 
         }
         closedir(dir);
     }

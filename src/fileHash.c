4cdcc5b0ad853c07db0564d45a477a432e78d4aa
helixTp <helixtp@yahoo.fr>
2019-02-18 20:42:54 +0100

    add enum Bool

--- src/fileHash.c
+++ src/fileHash.c
@@ -2,6 +2,7 @@
 // Created by DENIS LALLEMAND on 2019-02-18.
 //
 
+#include <stdlib.h>
 #include "fileHash.h"
 
 Hash new_hash(void){
--- src/fileHash.h
+++ src/fileHash.h
@@ -5,13 +5,19 @@
 #ifndef CPSAFETY_FILEHASH_H
 #define CPSAFETY_FILEHASH_H
 
+typedef enum
+{
+    false, //0
+    true //1
+}Bool;
+
 
 typedef struct fileHash{
     char filename[256];
     char path[1024];
     long long int hash;
     struct fileHash *next;
-}fileHash, Hash;
+}fileHash, *Hash;
 
 Hash new_hash(void);
 
